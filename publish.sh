86703135b0975f6bbc4379b5d3a2de4a5ef422b4
Oliver Schlegel <11634412+oschlegel@users.noreply.github.com>
2022-09-15 17:57:14 +0200

    fix(ghost-image): Whitespace blow image

--- publish.sh
+++ publish.sh
@@ -8,12 +8,11 @@ if [ "$VERSIONTYPE" == "" ]; then
     exit 1
 fi
 
-cd libs/ngx-ghosts
-npm version $VERSIONTYPE
+npm i
 
-cd ../..
+(cd libs/ngx-ghosts; npm version $VERSIONTYPE)
+
 npx ng run ngx-ghosts:build:production
 npx scss-bundle -c libs/ngx-ghosts/scss-bundle.config.json
 
-cd dist/libs/ngx-ghosts
-npm publish
+(cd dist/libs/ngx-ghosts; npm publish)
